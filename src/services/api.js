786bb8c44980769132da2afb05203ce3fbcc40d5
Prabhakar Verma <prabhakar8221@gmail.com>
2026-07-13 04:38:21 +0530

    Update API baseURL to Render backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://rent-easy-backend-abl7.onrender.com', 
 });
 
 instance.interceptors.request.use((config) => {
